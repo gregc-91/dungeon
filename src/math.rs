02981285a5da9ce0c5b51f0e25753c769c508785
Greg <41692114+gregc-91@users.noreply.github.com>
2020-06-16 19:51:02 +0100

    Adding simple Vec2 class

---
+++ src/math.rs
@@ -0,0 +1,44 @@
+
+use std::ops::{Add, Sub, Mul, Div};
+
+type Vec2i = Vec2<i32>;
+type Vec2u = Vec2<u32>;
+type Vec2f = Vec2<f32>;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vec2<T> {
+    pub x: T,
+    pub y: T
+}
+
+impl<T: Add<Output = T>> Add for Vec2<T> {
+    type Output = Vec2<T>;
+
+    fn add(self, rhs: Vec2<T>) -> Vec2<T> {
+        Vec2::<T> { x: self.x + rhs.x, y: self.y + rhs.y }
+    }
+}
+
+impl<T: Sub<Output = T>> Sub for Vec2<T> {
+    type Output = Vec2<T>;
+
+    fn sub(self, rhs: Vec2<T>) -> Vec2<T> {
+        Vec2::<T> { x: self.x - rhs.x, y: self.y - rhs.y }
+    }
+}
+
+impl<T: Mul<Output = T>> Mul for Vec2<T> {
+    type Output = Vec2<T>;
+
+    fn mul(self, rhs: Vec2<T>) -> Vec2<T> {
+        Vec2::<T> { x: self.x * rhs.x, y: self.y * rhs.y }
+    }
+}
+
+impl<T: Div<Output = T>> Div for Vec2<T> {
+    type Output = Vec2<T>;
+
+    fn div(self, rhs: Vec2<T>) -> Vec2<T> {
+        Vec2::<T> { x: self.x / rhs.x, y: self.y / rhs.y }
+    }
+}
