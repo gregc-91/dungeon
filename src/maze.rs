bd760d9a2cbdfb5d293a5ad949ab0c329e5ff915
Greg <41692114+gregc-91@users.noreply.github.com>
2020-06-22 21:24:04 +0100

    Added room generation

--- src/maze.rs
+++ src/maze.rs
@@ -12,7 +12,12 @@ impl Maze {
     pub fn new(width: usize, height: usize) -> Level {
         let mut level = Level::new(width, height);
         Maze::fill_perimeter(&mut level);
-        Maze::depth_first_walk(&mut level, (1,1));
+        Maze::gen_random_rooms(&mut level, 100);
+        for j in (1..height-1).step_by(2) {
+            for i in (1..width-1).step_by(2) {
+                Maze::gen_perfect_maze(&mut level, (i,j));
+            }
+        }
         return level;
     }
 
@@ -30,7 +35,7 @@ impl Maze {
         }
     }
 
-    fn depth_first_walk(level: &mut Level, (i, j): (usize, usize)) {
+    fn gen_perfect_maze(level: &mut Level, (i, j): (usize, usize)) {
         // Initialise the stack
         let mut stack: Vec<(usize, usize)> = Vec::new();
         stack.push((i, j));
@@ -42,7 +47,7 @@ impl Maze {
             if let Some((x, y)) = stack.pop() {
 
                 // Create a vector with valid options of where to step next
-                let next_cells = Maze::generate_options(level, (x, y));
+                let next_cells = Maze::test_neighbours(level, (x, y));
 
                 if next_cells.len() != 0 {
                     // Push this cell back onto the stack
@@ -65,14 +70,44 @@ impl Maze {
         }
     }
 
-    fn generate_options(level: &mut Level, (i, j): (usize, usize)) -> Vec<(isize, isize)> {
+    fn test_neighbours(level: &mut Level, (i, j): (usize, usize)) -> Vec<(isize, isize)> {
         let mut v: Vec<(isize, isize)> = Vec::new();
-
         if level[j][i-1].tile_type == TileType::_Empty && level[j][i-2].tile_type == TileType::_Empty { v.push((-1, 0)) };
         if level[j][i+1].tile_type == TileType::_Empty && level[j][i+2].tile_type == TileType::_Empty { v.push(( 1, 0)) };
         if level[j-1][i].tile_type == TileType::_Empty && level[j-2][i].tile_type == TileType::_Empty { v.push((0, -1)) };
         if level[j+1][i].tile_type == TileType::_Empty && level[j+2][i].tile_type == TileType::_Empty { v.push((0,  1)) };
-
         return v;
+    }
+
+    fn can_place_room(level: &Level, (x,y): (usize, usize), (w, h): (usize, usize)) -> bool {
+        for j in 0..h {
+            for i in 0..w {
+                if level[y+j][x+i].tile_type != TileType::_Empty {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    fn place_room(level: &mut Level, (x,y): (usize, usize), (w, h): (usize, usize)) {
+        for j in 0..h {
+            for i in 0..w {
+                level[y+j][x+i] = Tile{tile_type: TileType::_Floor, colour: Colour{r:128,g:128,b:128,a:255}};
+            }
+        }
+    }
+
+    fn gen_random_rooms(level: &mut Level, attempts: usize) {
+        for _a in 0..attempts {
+            let w = rand::thread_rng().gen_range(1, 5)*2+1;
+            let h = rand::thread_rng().gen_range(1, 4)*2+1;
+            let x = rand::thread_rng().gen_range(0, level.width>>1)*2+1;
+            let y = rand::thread_rng().gen_range(0, level.height>>1)*2+1;
+
+            if Maze::can_place_room(level, (x, y), (w, h)) {
+                Maze::place_room(level, (x, y), (w, h));
+            }
+        }
     }
 }
